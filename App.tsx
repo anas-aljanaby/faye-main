fdffca6e6ed8d94149adc9d1c89e7d74bf851cb9
anas-aljanaby <anas.aljanaby00@gmail.com>
2025-11-29 16:05:41 +0300

    edit functionality

--- App.tsx
+++ App.tsx
@@ -15,6 +15,8 @@ import Messages from './components/Messages';
 import HumanResources from './components/HumanResources';
 import SignIn from './components/SignIn';
 import ProtectedRoute from './components/ProtectedRoute';
+import TeamMemberRoute from './components/TeamMemberRoute';
+import SponsorProfile from './components/SponsorProfile';
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -35,15 +37,16 @@ function App() {
                     <div className="p-4 sm:p-6 md:p-8 flex-1">
                       <Routes>
                         <Route path="/" element={<Dashboard />} />
-                        <Route path="/financial-system" element={<FinancialSystem />} />
+                        <Route path="/financial-system" element={<TeamMemberRoute><FinancialSystem /></TeamMemberRoute>} />
                         <Route path="/orphan/:id" element={<OrphanProfile />} />
                         <Route path="/sponsor/:id" element={<SponsorPage />} />
                         <Route path="/team/:id" element={<TeamMemberPage />} />
                         <Route path="/orphans" element={<OrphansList />} />
-                        <Route path="/sponsors" element={<SponsorsList />} />
-                        <Route path="/team" element={<TeamList />} />
+                        <Route path="/sponsors" element={<TeamMemberRoute><SponsorsList /></TeamMemberRoute>} />
+                        <Route path="/team" element={<TeamMemberRoute><TeamList /></TeamMemberRoute>} />
                         <Route path="/messages" element={<Messages />} />
-                        <Route path="/human-resources" element={<HumanResources />} />
+                        <Route path="/human-resources" element={<TeamMemberRoute><HumanResources /></TeamMemberRoute>} />
+                        <Route path="/profile" element={<SponsorProfile />} />
                         <Route path="/policies" element={<div className="text-3xl font-bold">صفحة سياسات فيء</div>} />
                         <Route path="*" element={<Navigate to="/" replace />} />
                       </Routes>
