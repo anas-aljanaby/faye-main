690d6ff6e031eb32744a4278779f3258da3c8fb8
anas-aljanaby <anas.aljanaby00@gmail.com>
2026-03-01 16:47:04 +0300

    flip sidebar chevron

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -198,7 +198,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
             ${isCollapsed ? 'rotate-180' : ''}`}
             title={isCollapsed ? 'توسيع القائمة' : 'تصغير القائمة'}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m11 17-5-5 5-5"/><path d="m18 17-5-5 5-5"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="scale-x-[-1]"><path d="m11 17-5-5 5-5"/><path d="m18 17-5-5 5-5"/></svg>
           </button>
 
           <button 
